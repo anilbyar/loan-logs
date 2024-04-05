4c7405b42b3755a3d9adcb7274966a5726844c24
Anukul <anukulpokharel2058@gmail.com>
2024-04-05 19:10:01 +0530

    test

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { Button, Text, View } from 'react-native'
 export const HomeScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text>Anukul le banako ho yo</Text>
     </View>
   )
 }
