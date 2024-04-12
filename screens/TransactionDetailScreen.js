4d0c299e3c6f22060f76a142a95110a56c644b2c
Atul Chaudhary <atul.ac83@gmail.com>
2024-04-12 16:56:46 +0530

    every file was added and then committed

--- screens/TransactionDetailScreen.js
+++ screens/TransactionDetailScreen.js
@@ -1,7 +1,12 @@
 import { View } from 'react-native'
+import Icon from "react-native-vector-icons/dist/FontAwesome"
 
 export const TransactionDetailScreen = ({navigation}) => {
   return (
-    <Text>TransactionDetailScreen</Text>
+    
+      {/* top description */}
+      
+
+      
   )
 }
